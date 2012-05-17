4d38930ef71c118b985e6f54cc76d67558594341
Chadwick Boulay <chadwick.boulay@gmail.com>
2012-05-17 18:25:00 +0900

    NMESGUI supports either nmes device now.

--- addon/MyPythonApps/NMESGUI.py
+++ addon/MyPythonApps/NMESGUI.py
@@ -1,19 +1,72 @@
 from Tkinter import *
-from Caio.NMES import NMESFIFO
+
 import numpy as np
 import time
+
+def reset_frame(frame):
+    for ww in frame.pack_slaves():
+        ww.pack_forget()
+    #frame.grid_forget()
+    return frame
 
 class App:
     
     def __init__(self, master):
         #master is the root Tk element
-        self.nmes = NMESFIFO()
-        self.nmes.running = True
-            
-        self._amp = StringVar()
-        self._amp.set(str(self.nmes.amplitude))
-        
         button_frame = Frame(master)#Simple container
+        button_frame.pack(side = LEFT, fill = Y)
+        
+        self.dat_frame = Frame(master)
+        self.dat_frame.pack(side = LEFT, fill = Y)
+        
+        digitimer_button = Button(button_frame, text="DIGITIMER >>", command=self.digitimer)
+        digitimer_button.pack(side=TOP)
+        
+        hands_button = Button(button_frame, text="HANDS >>", command = self.hands)
+        hands_button.pack(side = TOP)
+        
+    def hands(self):
+        self.dat_frame=reset_frame(self.dat_frame)
+        HandsFrame(frame = self.dat_frame)
+    
+    def digitimer(self):
+        self.dat_frame=reset_frame(self.dat_frame)
+        DigitimerFrame(frame = self.dat_frame)
+        
+class HandsFrame:
+    def __init__(self, frame=None):
+        if not frame: frame=Toplevel()
+        self.frame = frame
+        
+        from Handbox.NMESInterface import NMES
+        self.frame.nmes = NMES(port='COM11')
+        
+        stop_button = Button(self.frame, text="STOP", command=self.frame.nmes.stop)
+        stop_button.pack(side = TOP, fill = X)
+        
+        GenericFrame(frame = self.frame)
+        
+class DigitimerFrame:
+    def __init__(self, frame=None):
+        if not frame: frame=Toplevel()
+        self.frame = frame
+        
+        from Caio.NMES import NMESFIFO
+        self.frame.nmes = NMESFIFO()
+        self.frame.nmes.running = True
+        
+        GenericFrame(frame = self.frame)
+        
+class GenericFrame:
+    def __init__(self, frame = None):
+        
+        if not frame: frame=Toplevel()
+        self.frame = frame
+        
+        self._int = StringVar()
+        self._int.set(str(self.frame.nmes.intensity))
+        
+        button_frame = Frame(self.frame)#Simple container
         button_frame.pack(side = TOP, fill = X)
         
         ppb = Button(button_frame, text="+1", command=self.plus_one)
@@ -25,26 +78,28 @@ class App:
         mmb = Button(button_frame, text="-1", command=self.minus_one)
         mmb.pack(side = RIGHT)
         
-        amp_label = Label(master, textvariable = self._amp)
+        amp_label = Label(self.frame, textvariable = self._int)
         amp_label.pack(side = TOP, fill = X)
         
-        zero_button = Button(master, text="0", command=self.zero)
+        zero_button = Button(self.frame, text="0", command=self.zero)
         zero_button.pack(side = BOTTOM, fill = X)
         
-        for i in np.arange(30):
-            self.plus_tenth()
-            time.sleep(0.1)
+        #for i in np.arange(30):
+        #    self.plus_tenth()
+        #    time.sleep(0.1)
     
-    def _change_amp(self, value):
-        self.nmes.amplitude = self.nmes.amplitude + float(value)
-        self._amp.set(str(self.nmes.amplitude))
-    def plus_one(self): self._change_amp(1)
-    def plus_tenth(self): self._change_amp(0.1)
-    def minus_tenth(self): self._change_amp(-0.1)
-    def minus_one(self): self._change_amp(-1)
+    def _change_int(self, value):
+        self.frame.nmes.intensity = self.frame.nmes.intensity + float(value)
+        self._int.set(str(self.frame.nmes.intensity))
+    def plus_one(self): self._change_int(1)
+    def plus_tenth(self): self._change_int(0.1)
+    def minus_tenth(self): self._change_int(-0.1)
+    def minus_one(self): self._change_int(-1)
     def zero(self):
-        self.nmes.amplitude = 0
-        self._amp.set(str(self.nmes.amplitude))
+        self.frame.nmes.intensity = 0
+        self._int.set(str(self.frame.nmes.intensity))
+        
+        #from Handbox.NMESInterface import NMES
         
 if __name__ == "__main__":
     root = Tk() #Creating the root widget. There must be and can be only one.
