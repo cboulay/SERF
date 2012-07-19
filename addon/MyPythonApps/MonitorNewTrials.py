8f22457321360bdb750b944496e742de4eadd199
Chadwick Boulay <chadwick.boulay@gmail.com>
2012-07-19 13:53:44 +0900

    Now monitors all channels.

--- addon/MyPythonApps/MonitorNewTrials.py
+++ addon/MyPythonApps/MonitorNewTrials.py
@@ -34,37 +34,47 @@ class App:
         if last_trial and not last_trial.store['channel_labels'] is None: #If new trial, add the trial to the plot
             tr_store=last_trial.store
             fig = self.fig
-            my_ax = fig.gca()
+            #my_ax = fig.gca()
             #my_ax.clear()
             #my_ax = fig.add_subplot(111)
-            chans_list = [tdv for tdv in last_trial.detail_values.itervalues() if tdv in tr_store['channel_labels']]
-            chans_list = list(set(chans_list))#make unique
+            #chans_list = [tdv for tdv in last_trial.detail_values.itervalues() if tdv in tr_store['channel_labels']]
+            #chans_list = list(set(chans_list))#make unique
             #Boolean array to index the data.
-            chan_bool = np.array([cl in chans_list for cl in tr_store['channel_labels']])
+            #chan_bool = np.array([cl in chans_list for cl in tr_store['channel_labels']])
             
-            x = last_trial.store['x_vec']
-            y = last_trial.store['data']
+            x = tr_store['x_vec']
+            y = tr_store['data']
             
             x_bool = np.logical_and(x>=-10,x<=100)
             x=x[x_bool]
-            y=y[chan_bool,x_bool]
+            #y=y[chan_bool,x_bool]
+            y=y[:,x_bool]
             
-            y_max = 0.0
-            y_min = 0.0
-            my_ax.lines = my_ax.lines[-4:]
-            my_ax.plot(x, y.T)
-            for ll in my_ax.lines:
-                ll.set_linewidth(0.5)
-                temp_data = ll.get_ydata()
-                y_min = min(y_min, min(temp_data[x>=10]))
-                y_max = max(y_max, max(temp_data[x>=10]))
-            my_ax.lines[-1].set_linewidth(3.0)
-            
-            #TODO: Scale y-axis to be +/- 20% around displayed trials (excluding stim artifact)
-            y_margin = 0.2 * np.abs((y_max - y_min))
-            my_ax.set_ylim(y_min-y_margin,y_max+y_margin)
-            my_ax.set_xlabel('TIME AFTER STIM (ms)')
-            my_ax.set_ylabel('AMPLITUDE (uv)')
+            nchans = np.size(tr_store['channel_labels'])
+            naxes = np.size(fig.axes)
+            while naxes<nchans:
+                fig.add_subplot(nchans,1,naxes+1)
+                naxes=np.size(fig.axes)
+                
+            for cc in range(0,nchans):
+                this_ax = fig.axes[cc]
+                this_ax.lines = this_ax.lines[-4:]
+                this_ax.plot(x,y[cc,:].T)
+                y_max = 0.0
+                y_min = 0.0
+                for ll in this_ax.lines:
+                    ll.set_linewidth(0.5)
+                    temp_data = ll.get_ydata()
+                    y_min = min(y_min, min(temp_data[x>=10]))
+                    y_max = max(y_max, max(temp_data[x>=10]))
+                this_ax.lines[-1].set_linewidth(3.0)
+                #TODO: Scale y-axis to be +/- 20% around displayed trials (excluding stim artifact)
+                y_margin = 0.2 * np.abs((y_max - y_min))
+                this_ax.set_ylim(y_min-y_margin,y_max+y_margin)
+                if cc==nchans-1:this_ax.set_xlabel('TIME AFTER STIM (ms)')
+                this_ax.set_ylabel('AMPLITUDE (uv)')
+                this_ax.set_title(tr_store['channel_labels'][cc])
+            fig.tight_layout()
             fig.canvas.draw()
             self.last_id = last_trial.datum_id
         
