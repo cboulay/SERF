e26732c83ca35fb768aecd360db92691863ebac6
Chadwick Boulay <chadwick.boulay@gmail.com>
2012-10-05 18:17:45 +0900

    Monitor multiple channels.

--- python/eerf/eerfd/static/eerfd/monitor.js
+++ python/eerf/eerfd/static/eerfd/monitor.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+		
     var options = {
         series: {
             lines: { show: true },
@@ -8,39 +9,47 @@ $(document).ready(function () {
         xaxis: { tickDecimals: 0 },
         selection: { mode: "x" }
     };
- 
-    var placeholder = $("#placeholder");
- 
-    placeholder.bind("plotselected", function (event, ranges) {
-        $("#selection").text(ranges.xaxis.from.toFixed(1) + " to " + ranges.xaxis.to.toFixed(1));
- 
-        var zoom = $("#zoom").attr("checked");
-        if (zoom)
-            plot = $.plot(placeholder, chaddata,
-                          $.extend(true, {}, options, {
-                              xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }
-                          }));
-    });
- 
-    placeholder.bind("plotunselected", function (event) {
-        $("#selection").text("");
-    });
     
-    var plot = $.plot(placeholder, chaddata, options);
- 
+    var placeholder=[];
+    var plot=[];
+    for (var i=0; i<channel_labels_s.length; i++){
+    	placeholder[i] = $(".placeholder."+channel_labels_s[i]);
+    	placeholder[i].bind("plotselected", function (event, ranges) {
+	        $("#selection").text(ranges.xaxis.from.toFixed(1) + " to " + ranges.xaxis.to.toFixed(1));
+	 
+	        var zoom = $("#zoom").attr("checked");
+	        if (zoom)
+	            plot[i] = $.plot(placeholder[i], mydata[channel_labels_s[i]],
+	                          $.extend(true, {}, options, {
+	                              xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }
+	                          }));
+	    });
+	 
+	    placeholder[i].bind("plotunselected", function (event) {
+	        $("#selection").text("");
+	    });
+	    
+	    plot[i] = $.plot(placeholder[i], mydata[channel_labels_s[i]], options);
+    };
+    
     $("#clearSelection").click(function () {
-        plot.clearSelection();
+    	for (var i=0; i<channel_labels.length; i++){
+    		plot[i].clearSelection();
+    	}
     });
     
     setInterval(
     	function () {
-	    	$.get("http://127.0.0.1:8000/eerfd/store_pk_check/" + pk, {}, function(pk_check) {
-	    		pk_check = JSON.parse(pk_check);
-	    		if (pk_check[1]>=5){
-	    			var new_pk = pk_check[0]-5;
-	    			window.location = "http://127.0.0.1:8000/eerfd/monitor/" + new_pk;
+    		//oldest_pk is a global giving the oldest_pk on the screen
+    		//if the page was loaded without an id, then oldest_pk will be the last trial in the db.
+	    	$.get("http://127.0.0.1:8000/eerfd/store_pk_check/" + oldest_pk, {}, function(n_stores) {
+	    		n_stores = JSON.parse(n_stores);
+	    		n_plotted = channel_labels_s.length>0 ? mydata[channel_labels_s[0]].length : 0;
+	    		if (n_stores>n_plotted){
+	    			window.location = "http://127.0.0.1:8000/eerfd/monitor/" + oldest_pk;
+	    			//this url will always give us the most recent 5 trials, and set oldest_pk to the oldest displayed
 	    		}
 			});
     	}
-	, 800);//milliseconds
+	, 700);//milliseconds
 });
--- python/eerf/eerfd/views.py
+++ python/eerf/eerfd/views.py
@@ -9,33 +9,60 @@ def index(request):
     return render_to_response('eerfd/index.html')
 
 def store_pk_check(request, pk):
+    #Given a pk, return how many DatumStore objects we have with pk greater
     pk = int(pk) if len(pk)>0 else 0
-    stores = DatumStore.objects.filter(pk__gt=pk).order_by('-pk')
-    n_stores = stores.count()
-    new_pk = stores[0].pk
-    result = [new_pk, n_stores]
-    return HttpResponse(json.dumps(result))
+    n_stores = DatumStore.objects.filter(pk__gte=pk).count()
+    return HttpResponse(json.dumps(n_stores))
 
-def monitor(request, pk):
-    #Shows n_erps most recent ERPs greater than pk.
-    n_erps = 5
-    pk = int(pk) if len(pk)>0 else None
-    if not pk:
-        pk = DatumStore.objects.order_by('-pk')[0].pk
-        data = '{}'
+def monitor(request, pk, n_erps = 5):
+    [x_min,x_max] = [-10.0,100.0]
+    #Shows most recent n_erps ERPs with pk greater than or equal to pk
+    pk = int(pk) if len(pk)>0 else 0#pk after which to get trial data
+    stores = DatumStore.objects.filter(pk__gte=pk).order_by('-pk')[0:n_erps]#trial data
+    #stores = [st for st in stores]#if using iterator()
+    #n_stores = len(stores)#how many trials with data were found
+    n_stores = stores.count()
+    #===========================================================================
+    # Situations:
+    # 1: We got here with pk=0 and there are no trials: no data, oldest_pk = 0. default.
+    # 2: We got here with pk=0 and there are some old trials: no data, oldest_pk is future pk
+    # 3: We got here with pk>0 and there are no trials (future pk): no data, oldest_pk is still future pk, also default
+    # 4: We got here with pk>0 and there are some trials: data, oldest_pk is that of the oldest returned trial
+    #===========================================================================
+    #data = [{ "label": pk, "data": [[-50,100],[0,0]]}]
+    data = []
+    oldest_pk = pk
+    channel_labels = []
+    if pk==0:
+        if n_stores==0:#1
+            pass
+        else:#2
+            oldest_pk = stores[0].pk + 1
     else:
-        stores = DatumStore.objects.filter(pk__gt=pk).order_by('-pk')[:5]#The first one is the newest
-        #=======================================================================
-        # Data should be transformed into the following JSON
-        # data = [{ label: pk, data: [[x,y],[x,y] ...]]}, {}...]
-        #=======================================================================
-        data = [{'label': st.pk, 'data': np.column_stack((st.x_vec.T,st.data.T[:,0])).tolist()} for st in stores]
-        #Don't need to remove the baseline.
-        #But should limit x.
-        pk = stores[stores.count()-1].pk
-        #Need the javascript to autorefresh whenever store_pk_check returns a value greater than pk
+        if n_stores==0:#3
+            pass
+        else:#4
+            n_channels = [st.n_channels for st in stores]
+            channel_labels = stores[np.nonzero(n_channels==np.max(n_channels))[0][0]].channel_labels
+            #data = [{"label": st.pk, "data": np.column_stack((st.x_vec.T,st.data.T[:,0])).tolist()} for st in stores]
+            #This next line is incredibly complicated. it's the equivalent of the following loops.
+            data = {}
+            for chlb in channel_labels:#for each channel
+                data[chlb] = []
+                for i in range(n_stores):#For each returned ERP
+                    st = stores[i]
+                    data[chlb].append({})
+                    data[chlb][i]['label'] = st.pk
+                    x_bool = np.logical_and(st.x_vec>=x_min,st.x_vec<=x_max)
+                    x_vals = st.x_vec[x_bool]
+                    y_vals = st.data[channel_labels.index(chlb),x_bool]
+                    data[chlb][i]['data'] = [x_vals, y_vals]
+                data[chlb].reverse()
+            data = dict([(chlb, [{'label': st.pk, 'data': np.column_stack((st.x_vec[np.logical_and(st.x_vec>=x_min,st.x_vec<=x_max)], st.data[channel_labels.index(chlb),np.logical_and(st.x_vec>=x_min,st.x_vec<=x_max)])).tolist()} for st in reversed(stores)]) for chlb in channel_labels])    
+            oldest_pk = stores[n_stores-1].pk
+            
     #return HttpResponse(json.dumps(data))
-    return render_to_response('eerfd/monitor.html', {'pk': pk, 'data': json.dumps(data)})
+    return render_to_response('eerfd/monitor.html', {'oldest_pk': oldest_pk, 'data': json.dumps(data), 'channel_labels': channel_labels, 'channel_labels_s': json.dumps(channel_labels)})
 
 def erps(request, trial_pk_csv='0'):
     #convert trial_pk_csv to trial_pk_list
